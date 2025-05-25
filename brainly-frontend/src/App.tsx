14b8a46ca440ea589393a3ee2a72e838af6c0479
pankaj singh <pankaj@upsidecloudtechnologies.com>
2025-05-25 23:08:11 +0530

    init

--- brainly-frontend/src/App.tsx
+++ brainly-frontend/src/App.tsx
@@ -5,10 +5,28 @@ import ShareIcon from "./Icons/ShareIcon";
 
 function App() {
   return (
-    <div>
-      <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon />}/>
-      <Button variant="primary" text="Add Content" startIcon={<PlusIcon />} />
-      <Card/>
+    <div className="p-4">
+      <div className="flex justify-end gap-4">
+        <Button
+          variant="secondary"
+          text="Share Brain"
+          startIcon={<ShareIcon />}
+        />
+        <Button variant="primary" text="Add Content" startIcon={<PlusIcon />} />
+      </div>
+
+      <div className="flex gap-4">
+        <Card
+          type="twitter"
+          link="https://twitter.com/pankaj_sgh/status/1921200837528850907"
+          title="First Post"
+        />
+        <Card
+          type="youtube"
+          link="https://www.youtube.com/embed/qcucgNV7Cmc"
+          title="Zor ka Jhatka"
+        />
+      </div>
     </div>
   );
 }
--- brainly-frontend/src/components/Card.tsx
+++ brainly-frontend/src/components/Card.tsx
@@ -1,24 +1,74 @@
 import ShareIcon from "../Icons/ShareIcon";
 
-function Card() {
+interface CardProps {
+  title: string;
+  link: string;
+  type: "twitter" | "youtube";
+}
+
+function Card({ title, link, type }: CardProps) {
   return (
     <div>
-      <div className="p-6 bg-white rounded-md border-gray-250 max-w-72 border ">
+      <div className="p-6 bg-white rounded-md border-gray-250 max-w-72 border min-h-48">
         <div className="flex justify-between">
           <div className="flex items-center text-md ">
             <div className="text-gray-500 pr-2 ">
               <ShareIcon />
             </div>
-            Project Ideas
+            {title}
           </div>
           <div className="flex">
             <div className="text-gray-500 pr-2 ">
-              <ShareIcon />
+              <a href={link} target="_blank">
+                <ShareIcon />
+              </a>
             </div>
             <div className="text-gray-500 ">
               <ShareIcon />
             </div>
           </div>
+        </div>
+
+        {/* Content Section */}
+        <div className="flex pt-8">
+          {type === "youtube" && (
+            <iframe
+              className="w-full"
+              width="860"
+              height="400"
+              src={link}
+              title="Zor Ka Jhatka Full HD Song Action Replayy"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          )}
+          {type === "twitter" && (
+            <div className="twitter-embed">
+              <blockquote className="twitter-tweet">
+                <p lang="en" dir="ltr">
+                  Today I explored Recoil in React and although it was confusing
+                  at first - I finally gained some handson experience and good
+                  understanding. Huge thanks to{" "}
+                  <a href="https://twitter.com/kirat_tw">@kirat_tw</a> and{" "}
+                  <a href="https://twitter.com/100xDevs">@100xDevs</a> for
+                  making it click! �{" "}
+                  <a href="https://twitter.com/hashtag/React">#React</a>{" "}
+                  <a href="https://twitter.com/hashtag/Recoil">#Recoil</a>{" "}
+                  <a href="https://twitter.com/hashtag/100xDevs">#100xDevs</a>{" "}
+                  <a href="https://twitter.com/hashtag/kirat_tw">#kirat_tw</a>{" "}
+                  <a href="https://t.co/zowonQR1N6">
+                    pic.twitter.com/zowonQR1N6
+                  </a>
+                </p>
+                &mdash; Pankaj (@pankaj_sgh){" "}
+                <a href={link}>
+                  May 10, 2025
+                </a>
+              </blockquote>
+            </div>
+          )}
         </div>
       </div>
     </div>
